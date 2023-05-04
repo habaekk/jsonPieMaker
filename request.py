e93b7f37182b81100cbce1f0d68bd3a1fe2380c5
habaek <haback1493@gmail.com>
2023-05-04 19:50:38 +0900

    implement request.py

--- request.py
+++ request.py
@@ -3,13 +3,7 @@ import xml.etree.ElementTree as ET
 
 from K import K
 
-def request():
-    keyManager = K()
-    key = keyManager.getKey()
-
-    urlFront = "https://apis.data.go.kr/B090041/openapi/service/LrsrCldInfoService/getLunCalInfo?"
-    testUrlQuery = "solYear=2023&solMonth=04&solDay=&ServiceKey=" + key
-    url = urlFront + testUrlQuery
+def request(url):
 
     # Make a GET request to the API endpoint
     response = requests.get(url)
@@ -18,16 +12,9 @@ def request():
     if response.status_code == 200:
         # Parse the XML data using ElementTree
         root = ET.fromstring(response.content)
-        
-        # Find a lunar day
-        body = root.find(".//body")
-        items = body.find(".//items")
-        item = body.find(".//item")
-        lunDay = item.find('.//lunDay')
-
-        print(lunDay.text)
-
+        return root
 
     else:
         # If the request was not successful, print an error message
         print('Error:', response.status_code)
+        return -1
