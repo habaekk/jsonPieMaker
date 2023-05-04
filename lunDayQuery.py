f60e9704df0e9f337b504e922483b803c59e7a66
habaek <haback1493@gmail.com>
2023-05-04 20:05:00 +0900

    implement lunDayQuery

--- lunDayQuery.py
+++ lunDayQuery.py
@@ -1,7 +1,28 @@
+import xml.etree.ElementTree as ET
+
+from urlHelper import urlHelper
+from request import request
+from XMLParser import XMLParser
 
 class LunDayQuery():
     def __init__(self):
-        self.lunDay = -1
-
-    def fetchLunDay(self):
-        return self.lunDay
+        pass
+    
+    def fetchLunDay(self, month, day, year):
+        url = self.getUrl(month, day, year)
+        xml = self.getXML(url)
+        lunDay = self.parse(xml)
+        
+        return lunDay
+    
+    def parse(self, xmlRoot):
+        lunD = XMLParser(xmlRoot)
+        return lunD
+    
+    def getXML(self, url):
+        xml = request(url)
+        return xml
+    
+    def getUrl(self, month, day, year): # fetch 랑 get 이랑 차이가 뭐냐?
+        url = urlHelper(month, day, year)
+        return url
